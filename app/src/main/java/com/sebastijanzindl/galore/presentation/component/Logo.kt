1836bd530e506db1e70a5f7943e31f2d1ec73e4a
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-04-05 00:17:03 +0200

    feat: progress on home design

--- app/src/main/java/com/sebastijanzindl/galore/presentation/component/Logo.kt
+++ app/src/main/java/com/sebastijanzindl/galore/presentation/component/Logo.kt
@@ -5,16 +5,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sebastijanzindl.galore.ui.theme.benzinFont
 
 @Preview
 @Composable
 fun Logo(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    fontSize: TextUnit? = null,
 ) {
     Text(text = "galore",
-        fontSize = 48.sp,
+        fontSize = fontSize ?: 48.sp,
         fontFamily = benzinFont,
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier
--- app/src/main/java/com/sebastijanzindl/galore/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/sebastijanzindl/galore/presentation/screens/home/HomeScreen.kt
@@ -5,42 +5,85 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.sebastijanzindl.galore.presentation.component.Logo
 import com.sebastijanzindl.galore.ui.theme.GaloreTheme
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
-fun HomeScreen (
+fun HomeScreen(
     modifier: Modifier = Modifier,
-    viewModel: HomeScreenViewModel = hiltViewModel(),
+//    viewModel: HomeScreenViewModel = hiltViewModel(),
     navigateToAuth: () -> Unit,
-){
-
+) {
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showBottomSheet by remember {
+        mutableStateOf(false)
+    }
     val scrollBehaviour = TopAppBarDefaults.enterAlwaysScrollBehavior()
-    Scaffold (
+
+    val openBottomSheet = {
+        showBottomSheet = true
+    }
+
+    Scaffold(
         modifier = modifier.nestedScroll(scrollBehaviour.nestedScrollConnection),
         topBar = {
             HomeTopAppBar(
-                scrollBehaviour = scrollBehaviour
+                scrollBehaviour = scrollBehaviour,
+                openBottomSheet = openBottomSheet
             )
         }
-    ) {contentPadding ->
+    ) { contentPadding ->
         Button(modifier = Modifier.padding(top = contentPadding.calculateTopPadding()), onClick = {
-            viewModel.logout(navigateToAuth)
+//            viewModel.logout(navigateToAuth)
         }) {
             Text(text = "Logout")
+        }
+
+        if(showBottomSheet) {
+            ModalBottomSheet(onDismissRequest = {
+                showBottomSheet = false
+            }, sheetState = sheetState) {
+                Button(onClick = {
+                    scope.launch { sheetState.hide() }.invokeOnCompletion {
+                        if (!sheetState.isVisible) {
+                            showBottomSheet = false
+                        }
+                    }
+                }) {
+                    Text("Hide bottom sheet")
+                }
+            }
         }
 
     }
@@ -52,25 +95,27 @@ fun HomeScreen (
 @Composable
 private fun HomeTopAppBar(
     modifier: Modifier = Modifier,
-    scrollBehaviour: TopAppBarScrollBehavior
+    scrollBehaviour: TopAppBarScrollBehavior,
+    openBottomSheet: () -> Unit
 ) {
-    CenterAlignedTopAppBar(title =
-    {
-        Row (
-            Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Text(text = "Galore", style=MaterialTheme.typography.displaySmall)
-        }
-    },
-        scrollBehavior = scrollBehaviour,
-        modifier = modifier,)
+    TopAppBar(title = {
+                      Row (
+                          Modifier.fillMaxWidth(),
+                          horizontalArrangement = Arrangement.SpaceBetween,
+                          verticalAlignment = Alignment.CenterVertically
+                      ) {
+                          Logo(fontSize = 24.sp)
+                         IconButton(onClick = { openBottomSheet() }) {
+                             Icon(Icons.Filled.AccountCircle, contentDescription = "Account")
+                         }
+                      }
+    }, scrollBehavior = scrollBehaviour)
 }
 
 @Preview(apiLevel = 33)
 @Composable
 private fun HomeScreenPreview() {
     GaloreTheme {
-        HomeScreen(viewModel = hiltViewModel<HomeScreenViewModel>(), navigateToAuth =  {})
+        HomeScreen(navigateToAuth = {})
     }
 }
