25ac2192ebe02058bcbea224812434f010158d3d
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-04-14 21:59:53 +0200

    feat: logic added for if a cocktail is in favourites

--- app/src/main/java/com/sebastijanzindl/galore/presentation/component/CocktailCard.kt
+++ app/src/main/java/com/sebastijanzindl/galore/presentation/component/CocktailCard.kt
@@ -1,7 +1,6 @@
 package com.sebastijanzindl.galore.presentation.component
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,19 +9,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -31,10 +24,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sebastijanzindl.galore.R
@@ -46,7 +37,8 @@ import kotlinx.datetime.LocalDate
 fun  CocktailCard (
     cocktail: Cocktail,
     onHeartPress: () -> Unit,
-    onCardPress: () -> Unit
+    onCardPress: () -> Unit,
+    isFavourite: Boolean
 ) {
 
     OutlinedCard(
@@ -82,7 +74,12 @@ fun  CocktailCard (
                 modifier = Modifier.size(32.dp),
                 onClick = onHeartPress
             ) {
-                Icon(Icons.Default.FavoriteBorder, contentDescription = "Add to Favourite", Modifier.padding(8.dp))
+                if(isFavourite) {
+                    Icon(Icons.Default.Favorite, contentDescription = "Add to Favourite", modifier = Modifier.padding(8.dp))
+                } else {
+                    Icon(Icons.Default.FavoriteBorder, contentDescription = "Remove From Favourites", modifier = Modifier.padding(8.dp))
+                }
+
             }
         }
 
@@ -114,6 +111,7 @@ private fun CocktailCardPreview() {
             cocktail = cocktail,
             onCardPress = {},
             onHeartPress = changeIsFavourite,
+            isFavourite = isFavourite
         )
     }
 }
