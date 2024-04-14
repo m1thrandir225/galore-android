bf314054f08ee590f4f38dd9cb80ea684e3561b1
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-04-14 21:57:14 +0200

    feat: cocktail card design finished

--- app/src/main/java/com/sebastijanzindl/galore/presentation/component/CocktailCard.kt
+++ app/src/main/java/com/sebastijanzindl/galore/presentation/component/CocktailCard.kt
@@ -1,13 +1,42 @@
 package com.sebastijanzindl.galore.presentation.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sebastijanzindl.galore.R
 import com.sebastijanzindl.galore.domain.models.Cocktail
 import com.sebastijanzindl.galore.ui.theme.GaloreTheme
@@ -19,13 +48,43 @@ fun  CocktailCard (
     onHeartPress: () -> Unit,
     onCardPress: () -> Unit
 ) {
+
     OutlinedCard(
         onClick = onCardPress,
-
+        modifier = Modifier.width(200.dp)
     ) {
-        Image(painter = painterResource(id = R.drawable.google_logo), contentDescription = "")
-        Text(text = "Hello World");
-        Text(text = "Hello!")
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(166.dp),
+            painter = painterResource(id = R.drawable.cocktail_card_image_preview), contentDescription = "",
+            contentScale = ContentScale.Crop
+        )
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp)
+                ,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column (
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalAlignment = Alignment.Start,
+            ) {
+                Text(
+                    text = cocktail.name,
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+            FilledIconButton(
+                modifier = Modifier.size(32.dp),
+                onClick = onHeartPress
+            ) {
+                Icon(Icons.Default.FavoriteBorder, contentDescription = "Add to Favourite", Modifier.padding(8.dp))
+            }
+        }
 
     }
 }
@@ -34,6 +93,13 @@ fun  CocktailCard (
 @Preview
 @Composable
 private fun CocktailCardPreview() {
+    var isFavourite by remember {
+        mutableStateOf(false)
+    }
+
+    val changeIsFavourite = {
+        isFavourite = !isFavourite
+    }
     GaloreTheme {
         val cocktail = Cocktail(
             id = "1",
@@ -47,7 +113,7 @@ private fun CocktailCardPreview() {
         CocktailCard(
             cocktail = cocktail,
             onCardPress = {},
-            onHeartPress = {},
+            onHeartPress = changeIsFavourite,
         )
     }
 }
