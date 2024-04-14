3c6bd1b1ee5101bc5abd32f0ac54030d43893e4c
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-04-14 22:11:44 +0200

    feat: async image from coil to load images

--- app/src/main/java/com/sebastijanzindl/galore/presentation/component/CocktailCard.kt
+++ app/src/main/java/com/sebastijanzindl/galore/presentation/component/CocktailCard.kt
@@ -1,6 +1,5 @@
 package com.sebastijanzindl.galore.presentation.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -25,9 +24,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.sebastijanzindl.galore.R
 import com.sebastijanzindl.galore.domain.models.Cocktail
 import com.sebastijanzindl.galore.ui.theme.GaloreTheme
@@ -45,12 +47,16 @@ fun  CocktailCard (
         onClick = onCardPress,
         modifier = Modifier.width(200.dp)
     ) {
-        Image(
-            modifier = Modifier
-                .fillMaxWidth()
+        AsyncImage(
+            modifier = Modifier.fillMaxWidth()
                 .height(166.dp),
-            painter = painterResource(id = R.drawable.cocktail_card_image_preview), contentDescription = "",
-            contentScale = ContentScale.Crop
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(cocktail.image)
+                .crossfade(true)
+                .build(),
+            contentDescription = "Image of the cocktail: ${cocktail.name}",
+            placeholder = painterResource(id = R.drawable.cocktail_card_image_preview), //TODO: Replace with actually placeholder
+            contentScale = ContentScale.Crop,
         )
         Row (
             modifier = Modifier
@@ -86,7 +92,6 @@ fun  CocktailCard (
     }
 }
 
-
 @Preview
 @Composable
 private fun CocktailCardPreview() {
@@ -100,7 +105,7 @@ private fun CocktailCardPreview() {
     GaloreTheme {
         val cocktail = Cocktail(
             id = "1",
-            image = "",
+            image = "https://images.unsplash.com/photo-1712928247899-2932f4c7dea3?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             createdAt = LocalDate.parse("2024-02-24").toString(),
             embeddingVector = listOf(0.10, 1.22, 1.55),
             ingredients = "Gin, Tonic",
