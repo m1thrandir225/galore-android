c0f0bb6082ca9e0750ae324e7d1a96b015c34a2a
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-04-22 21:12:37 +0200

    feat: limit profile bottom sheet max height

--- app/src/main/java/com/sebastijanzindl/galore/presentation/component/ProfileBottomSheet.kt
+++ app/src/main/java/com/sebastijanzindl/galore/presentation/component/ProfileBottomSheet.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
@@ -44,9 +45,10 @@ fun ProfileBottomSheet(
     menuItems: @Composable () -> Unit,
 ) {
     ModalBottomSheet(
-        modifier = modifier,
+        modifier = modifier.heightIn(max = 450.dp),
         onDismissRequest = onDismissRequest,
-        sheetState = sheetState
+        sheetState = sheetState,
+
     ) {
         Column(
             modifier = Modifier.fillMaxSize(),
